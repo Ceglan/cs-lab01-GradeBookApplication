e8a359c1edb32d73a02ff861ec70eb8c420842d9
TomBu <TomBu@FINDOR777>
2024-04-07 16:49:17 +0200

    Add support for weighted GPAs

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -98,6 +98,7 @@ namespace GradeBook.GradeBooks
     {
         public string Name { get; set; }
         public List<Student> Students { get; set; }
+        public bool IsWeighted { get; set; }
 
         public BaseGradeBook(string name)
         {
